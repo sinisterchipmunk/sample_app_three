69d26475b639358e9fcaf1ba0b2ce34a66656e57
Bobby Sudekum <rsudekum@gmail.com>
2012-07-14 16:33:02 -0700

    Finish user edit index and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,30 +2,43 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  valid_email_regex = /\A[\w+\-.]+@[\w\-.]+\.[a-z]+\z/i
+  # \A    Start of string 
+  # \w    Any word character (letter, number, underscore)
+  # [a-z] Any single character in the range a-z
+  # \z    End of string
+  # i     Case insensitive
+  # Note: Everything before the +@ and before the +\. will verify that the first portion of the email address  
+  #       will only contain letters, numbers, underscores, hyphens and periods.  Everything after the +\. will 
+  #       only contain letters.
   validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
+                    format:     { with: valid_email_regex },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-private
+  
+  private
 
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
     end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime        not null
+#  updated_at      :datetime        not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
+#
+
 require 'spec_helper'
 
 describe User do
@@ -14,32 +28,34 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) } 
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
-  describe "remember token" do
-      before { @user.save }
-      its(:remember_token) { should_not be_blank }
-    end
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-  
+
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
-  
+
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-  
+
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
@@ -63,15 +79,13 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-			user_with_same_email.email = @user.email.upcase
+      user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
 
     it { should_not be_valid }
   end
-  
-  it { should respond_to(:authenticate) }
-  
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -81,7 +95,7 @@ describe User do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
-  
+
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
@@ -107,4 +121,10 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 end
